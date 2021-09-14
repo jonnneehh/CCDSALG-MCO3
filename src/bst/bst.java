3c913d3381ee0c1e618841b3a1e5bec8ecf841e7
jonnneehh <you@example.com>
2021-09-14 21:58:45 +0800

    Added insert

--- src/bst/bst.java
+++ src/bst/bst.java
@@ -7,14 +7,12 @@ public class bst {
     public bst(){
          root = null;
     }
- 
-    void insert(int key){
+    
+    public void insert(int key){
          root = insertRec(root, key);
     }
  
-    node insertRec(node root, int key)
-    {
- 
+    private node insertRec(node root, int key){
         if (root == null){
             root = new node(key);
             return root;
@@ -27,15 +25,26 @@ public class bst {
         return root;
     }
  
-    void inorder(){
+    public void inorder(){
          inorderRec(root);
     }
  
-    void inorderRec(node root){
+    private void inorderRec(node root){
         if (root != null) {
             inorderRec(root.left);
             System.out.println(root.key);
             inorderRec(root.right);
         }
+    }
+    
+    public node search(node root, int key)
+    {
+        if (root==null || root.key==key)
+            return root;
+     
+        if (root.key < key)
+           return search(root.right, key);
+     
+        return search(root.left, key);
     }
 }
