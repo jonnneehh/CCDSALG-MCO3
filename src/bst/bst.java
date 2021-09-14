eeccf73e2e6f742a29d88de4286f5655b41e78a3
jonnneehh <you@example.com>
2021-09-14 21:38:04 +0800

    Added prototype bst and node.java

--- src/bst/bst.java
+++ src/bst/bst.java
@@ -1,5 +1,41 @@
 package bst;
 
 public class bst {
-	
+ 
+    node root;
+
+    public bst(){
+         root = null;
+    }
+ 
+    void insert(int key){
+         root = insertRec(root, key);
+    }
+ 
+    node insertRec(node root, int key)
+    {
+ 
+        if (root == null){
+            root = new node(key);
+            return root;
+        }
+
+        if (key < root.key)
+            root.left = insertRec(root.left, key);
+        else if (key > root.key)
+            root.right = insertRec(root.right, key);
+        return root;
+    }
+ 
+    void inorder(){
+         inorderRec(root);
+    }
+ 
+    void inorderRec(node root){
+        if (root != null) {
+            inorderRec(root.left);
+            System.out.println(root.key);
+            inorderRec(root.right);
+        }
+    }
 }
