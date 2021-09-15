c69dffc66f3bd17d09c3283257729d3e2c8e2938
jonnneehh <you@example.com>
2021-09-15 23:06:48 +0800

    almost done w delete

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,18 +3,10 @@ import bst.bst;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void Main(String[] args) {
 		
 		bst.node root = null; 
 		
-<<<<<<< HEAD:src/main/main.java
-		root = bst.insert(root, 10);
-		root = bst.insert(root, 20);
-		root = bst.insert(root, 1000000);
-		bst.inorder(root);
-		System.out.print("\n");
-		bst.destroy(root);
-=======
 		root = bst.insert(root, 35);
 		root = bst.insert(root, 30);
 		root = bst.insert(root, 40);
@@ -22,7 +14,6 @@ public class Main {
 		System.out.print("\n");
 		root = bst.destroy(root);
 		System.out.print("\n");
->>>>>>> e8b5d756dae952d9b2d06663dbf1838ebfc596f2:src/main/Main.java
 		bst.inorder(root);
 	}
 }
