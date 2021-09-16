0545f8b79b74d3e2f4549cceb5a97bc8058c789a
MraccraM <sibalmarcl@gmail.com>
2021-09-16 12:55:02 +0800

    done

--- src/bst/WordCounter.java
+++ src/bst/WordCounter.java
@@ -33,18 +33,11 @@ public class WordCounter {
         Map<Object, Integer> mp = new HashMap<>();
         outputFile = new FileWriter("WORDS.txt");
 
-        // Traverse through array elements and
-        // count frequencies
         for (int i = 0; i < n; i++)
         {
             mp.put(arr[i], mp.get(arr[i]) == null ? 1 : mp.get(arr[i]) + 1);
         }
 
-        // To print elements according to first
-        // occurrence, traverse array one more time
-        // print frequencies of elements and mark
-        // frequencies as -1 so that same element
-        // is not printed multiple times.
         for (int i = 0; i < n; i++)
         {
             if (mp.get(arr[i]) != -1)
