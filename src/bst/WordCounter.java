6c279c153b98618003f600055152d187673889fa
MraccraM <sibalmarcl@gmail.com>
2021-09-16 11:56:37 +0800

    done

--- src/bst/WordCounter.java
+++ src/bst/WordCounter.java
@@ -5,13 +5,15 @@ import java.util.*;
 
 public class WordCounter {
     public ArrayList<String> words;
+    private Object[] strings;
+    private File inputFile;
+    private FileWriter outputFile;
 
     public WordCounter() throws IOException {
         bst.node root= null;
         words = new ArrayList<>();
 
-        File inputFile = new File("inputFile.txt");
-        FileWriter outputFile = new FileWriter("outputFile.txt");
+        inputFile = new File("inputFile.txt");
 
         Scanner scan = new Scanner(inputFile);
 
@@ -21,8 +23,35 @@ public class WordCounter {
 
         bst.inorder(this,root);
 
-        for (String word : words) {
-            outputFile.write(word + "\n");
+        strings = words.toArray();
+
+
+        countFreq(strings, strings.length);
+    }
+
+    public void countFreq(Object arr[], int n) throws IOException {
+        Map<Object, Integer> mp = new HashMap<>();
+        outputFile = new FileWriter("outputFile.txt");
+
+        // Traverse through array elements and
+        // count frequencies
+        for (int i = 0; i < n; i++)
+        {
+            mp.put(arr[i], mp.get(arr[i]) == null ? 1 : mp.get(arr[i]) + 1);
+        }
+
+        // To print elements according to first
+        // occurrence, traverse array one more time
+        // print frequencies of elements and mark
+        // frequencies as -1 so that same element
+        // is not printed multiple times.
+        for (int i = 0; i < n; i++)
+        {
+            if (mp.get(arr[i]) != -1)
+            {
+                outputFile.write(arr[i] + " " + mp.get(arr[i]) + "\n");
+                mp.put(arr[i], -1);
+            }
         }
         outputFile.close();
     }
